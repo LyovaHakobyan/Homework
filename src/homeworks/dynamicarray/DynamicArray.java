d822f26e38580ea1df1caa9be53238dcbac3200f
Lyova Hakobyan <lyov123lyov@gmail.com>
2023-09-18 14:18:37 +0400

    Homework1

--- src/homeworks/dynamicarray/DynamicArray.java
+++ src/homeworks/dynamicarray/DynamicArray.java
@@ -9,7 +9,7 @@ public class DynamicArray {
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
     public void add(int value) {
-        if (size == array.length) {
+        if (size == array.length - 1) {
             extend();
         }
         array[size++] = value;
