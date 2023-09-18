ecfb26da9000873c3b579755b9fbbc4008386c9e
Lyova Hakobyan <lyov123lyov@gmail.com>
2023-09-18 12:53:40 +0400

    Homework1

--- src/homeworks/homework4/Sort.java
+++ src/homeworks/homework4/Sort.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array = {4, 7, 1, 3, 9, 0, 2, -9, 648, -941, 0, 68481};
+        int[] array = {68481, 4, 7, 1, 3, 9, 0, 2, -9, 648, -941, 0, -999};
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int c = array[j];
                     array[j] = array[j + 1];
