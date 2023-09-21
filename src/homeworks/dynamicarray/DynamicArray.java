d8f2140b7ae4d7334fbf81170682a4cc17ca1276
Lyova Hakobyan <lyov123lyov@gmail.com>
2023-09-21 21:13:18 +0400

    Homework

--- src/homeworks/dynamicarray/DynamicArray.java
+++ src/homeworks/dynamicarray/DynamicArray.java
@@ -42,6 +42,67 @@ public class DynamicArray {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
     }
 
+    //ջնջել մասիվի index-երորդ էլեմենտը
+    //եթե չկա նման ինդեքսով էլեմենտ, տպել, որ չկա նման էլեմենտ
+    public void deleteByIndex(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        } else {
+            System.out.println("There is not any element under this index");
+        }
+    }
+
+    //տրված value-ն դնելու ենք տրված index-ով էլեմենտի տեղը։
+    //Հին արժեքը կկորի
+    //եթե նման ինդեքսով էլեմենտ չկա, գրում ենք որ չկա։
+    public void set(int index, int value) {
+        if (index >= 0 && index < size) {
+            array[index] = value;
+        } else {
+            System.out.println("There is not any element under this index");
+        }
+    }
+
+    //ավելացնել տրված value-ն տրված ինդեքսում, իսկ էղած էլեմենտները մի հատ աջ տանել։
+    //Եթե չկա նման ինդեքս, տպել որ չկա
+    public void add(int index, int value) {
+        if (index >= 0 && index < size) {
+            size++;
+            for (int i = size; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = value;
+        } else {
+            System.out.println("This index doesn't exist");
+        }
+    }
+
+    //Վերադարձնել true եթե տրված value-ն կա մեր մասիվի մեջ, եթե ոչ false
+    public boolean exists(int value) {
+        int temp = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                temp++;
+                break;
+            }
+        }
+        return temp > 0;
+    }
+
+    //Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+    //եթե չկա, -1
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/homeworks/dynamicarray/DynamicArrayDemo.java
+++ src/homeworks/dynamicarray/DynamicArrayDemo.java
@@ -6,16 +6,29 @@ public class DynamicArrayDemo {
         for (int i = 0; i < 10; i++) {
             da.add(i);
         }
+        da.add(99);
         da.print();
-        da.add(77);
-        System.out.println();
+        da.deleteByIndex(0);
         da.print();
-        System.out.println();
-        int temp = da.getByIndex(99);
-        System.out.println(temp);
-        da.add(787);
+        da.set(9, 777);
         da.print();
-        System.out.println();
-        System.out.println(da.getByIndex(5));
+        da.add(0, 888);
+        da.print();
+        da.add(11, 888);
+        da.print();
+        System.out.println(da.exists(777));
+        da.add(2, 888);
+        da.add(0, 646);
+        da.print();
+        System.out.println(da.getIndexByValue(1));
+        System.out.println(da.exists(1));
+        da.add(16);
+        da.print();
+        da.set(13, 600);
+        da.print();
+        da.add(0, 444);
+        da.print();
+        da.deleteByIndex(13);
+        da.print();
     }
 }
